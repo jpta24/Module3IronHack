7c662393e12bb69e2b8143d8ea74a2cbe6e36325
Jpta24 <jeanpierretorres@gmail.com>
2022-09-26 17:35:21 +0200

    Jp work tasks routes

--- w8d1/project-management-app/project-management-server/routes/task.routes.js
+++ w8d1/project-management-app/project-management-server/routes/task.routes.js
@@ -1,19 +1,48 @@
-const router = require("express").Router();
+const router = require('express').Router();
 // const mongoose = require('mongoose');
- 
+
 const Task = require('../models/Task.model');
 const Project = require('../models/Project.model');
- 
+
 //  POST /api/tasks  -  Creates a new task
 router.post('/tasks', (req, res, next) => {
-  const { title, description, projectId } = req.body;
- 
-  Task.create({ title, description, project: projectId })
-    .then(newTask => {
-      return Project.findByIdAndUpdate(projectId, { $push: { tasks: newTask._id } }, {new: true} );
-    })
-    .then(project => res.json(project))
-    .catch(err => res.json(err));
+	const { title, description, projectId } = req.body;
+
+	Task.create({ title, description, project: projectId })
+		.then((newTask) => {
+			return Project.findByIdAndUpdate(
+				projectId,
+				{ $push: { tasks: newTask._id } },
+				{ new: true }
+			);
+		})
+		.then((project) => res.json(project))
+		.catch((err) => res.json(err));
 });
- 
+
+router.put('/tasks/:taskId', (req, res, next) => {
+  const { taskId } = req.params;
+	const { title, description } = req.body;
+
+	Task.findByIdAndUpdate(taskId, { title, description }, { new: true })
+		.then(task => res.json(task))
+    .catch(err => console.log(err))
+});
+
+router.delete('/tasks/:taskId',(req, res) => {
+  const { taskId } = req.params;
+  Task.findByIdAndRemove(taskId)
+        .then(task => {
+          Project.findByIdAndUpdate(task.project, {$pull: {tasks:taskId}}, {new:true})
+        })
+        .then((project)=> res.json({message: `Task ${taskId} was successfully deleted and the Project ${project.id} was updated`}))
+        .catch(err => console.log(err))
+
+})
+
+router.get('/tasks',(req, res) => {
+  Task.find()
+  .then(tasks => res.json(tasks))
+} )
+
 module.exports = router;
